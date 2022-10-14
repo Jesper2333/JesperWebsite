4def578f11b21344922b61270f1d18d713fd67cd
Jesper2333 <328394443@qq.com>
2022-10-14 16:04:15 +0800

    init repo

---
+++ src/main/java/com/example/jesperweb/JesperwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.jesperweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JesperwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JesperwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jesperweb/JesperwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jesperweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JesperwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
