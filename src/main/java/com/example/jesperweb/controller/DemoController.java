a8e7ea645389bbf9b2e6c0fa7c6ede95a8774203
Jesper2333 <328394443@qq.com>
2022-10-17 09:39:34 +0800

    Update:Demo

--- src/main/java/com/example/jesperweb/controller/DemoController.java
+++ src/main/java/com/example/jesperweb/controller/DemoController.java
@@ -1,12 +1,15 @@
 package com.example.jesperweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DemoController {
     @GetMapping("/Demo")
-    public String Demo(){
-        return null;
+    public String Demo(@RequestParam(name = "login")String login, Model Demo){
+        Demo.addAttribute("login",login);
+        return "Demo";
     }
 }
