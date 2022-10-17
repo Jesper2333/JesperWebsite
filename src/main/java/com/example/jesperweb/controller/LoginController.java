b95d14acf1169e91429b96bba3e7cbfc1632b9e9
Jesper2333 <328394443@qq.com>
2022-10-17 14:55:46 +0800

    Update:Bootstrap

--- src/main/java/com/example/jesperweb/controller/LoginController.java
+++ src/main/java/com/example/jesperweb/controller/LoginController.java
@@ -1,4 +1,12 @@
 package com.example.jesperweb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+    @GetMapping("/login")
+    private static String login(){
+        return "login";
+    }
 }
